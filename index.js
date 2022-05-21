45b4a524550803c47ed09947bf7ba984040472df
Edubew <edubew98@gmail.com>
2022-05-22 02:43:40 +0300

    Preserve data in the browser's memory using local storage

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let booksArray = JSON.parse(localStorage.getItem("booksArray")) || [];
+let booksArray = JSON.parse(localStorage.getItem('booksArray')) || [];
 
-let books = document.getElementById("books-inner-container");
-let addBtn = document.querySelector("#add-btn");
-let inputTitle = document.querySelector("#input-title");
-let inputAuthor = document.querySelector("#input-author");
-const form = document.querySelector("#form");
+const books = document.getElementById('books-inner-container');
+const addBtn = document.querySelector('#add-btn');
+const inputTitle = document.querySelector('#input-title');
+const inputAuthor = document.querySelector('#input-author');
+const form = document.querySelector('#form');
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   booksArray.title = inputTitle.value;
   booksArray.author = inputAuthor.value;
-  if (inputTitle.value != "" && inputAuthor.value != "") {
+  if (inputTitle.value !== '' && inputAuthor.value !== '') {
     const book = {
       id: new Date().getTime(),
       title: inputTitle.value,
       author: inputAuthor.value,
     };
     booksArray.push(book);
-    localStorage.setItem("booksArray", JSON.stringify(booksArray))
+    localStorage.setItem('booksArray', JSON.stringify(booksArray));
     addBook(book);
-    inputTitle.value = "";
-    inputAuthor.value = "";
+    inputTitle.value = '';
+    inputAuthor.value = '';
   }
 });
 
 let addBook = (book) => {
-  const bookElement = document.createElement("li");
-  bookElement.setAttribute("id", book.id);
+  const bookElement = document.createElement('li');
+  bookElement.setAttribute('id', book.id);
   const taskElMarkup = `
     <div class="content-container">
     <p>${book.title}</p>
@@ -38,21 +38,20 @@ let addBook = (book) => {
   books.appendChild(bookElement);
 };
 
+books.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-book') || e.target.parentElement.classList.contains('remove-book')) {
+    const bookId = e.target.closest('li').id;
+    removeBook(bookId);
+  }
+});
 
-books.addEventListener('click', (e) =>{
-    if(e.target.classList.contains("remove-book") || e.target.parentElement.classList.contains("remove-book")){
-        let bookId = e.target.closest("li").id
-        removeBook(bookId);
-    }
-})
+let removeBook = (id) => {
+  booksArray = booksArray.filter((book) => book.id !== parseInt(id, 2));
+  localStorage.setItem('booksArray', JSON.stringify(booksArray));
+  document.getElementById(id).remove();
+};
 
-let removeBook = (id) =>{
-    booksArray = booksArray.filter(book => book.id !== parseInt(id));
-    localStorage.setItem("booksArray", JSON.stringify(booksArray));
-    document.getElementById(id).remove();
-}
- 
-if (localStorage.getItem("booksArray")) {
+if (localStorage.getItem('booksArray')) {
   booksArray.map((book) => {
     addBook(book);
   });
